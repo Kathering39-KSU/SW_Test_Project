94e4f91ffde5778aa11b6c2d8b2be67f5e456f3f
Katherine Gibson <kgibso39@students.kennesaw.edu>
2024-02-07 19:17:57 -0500

    Figuring out C#

---
+++ Assignment1/Assignment1/Assignment2.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Assignment2
+{
+
+
+    public class Assignment2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test this");
+        }
+
+    }
+}
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");  
+            Print();
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
