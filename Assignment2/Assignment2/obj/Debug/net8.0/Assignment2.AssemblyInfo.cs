15ae6513c9a162a04111b869859e087d4cde14ee
Katherine Gibson <kgibso39@students.kennesaw.edu>
2024-02-12 18:34:07 -0500

    testbranch???

--- Assignment2/Assignment2/obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
+++ Assignment2/Assignment2/obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e4f91ffde5778aa11b6c2d8b2be67f5e456f3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
